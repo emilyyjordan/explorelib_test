f8b37cb9b55a576fed952bd604d83003c87cabca
emily jordan <78192360+emilyyjordan@users.noreply.github.com>
2023-10-13 12:47:29 -0400

    Update local_gym.py
    added seedling to init

--- explorationlib/local_gym.py
+++ explorationlib/local_gym.py
@@ -202,6 +202,7 @@ class BanditAddictive2(BanditEnv):
      def __init__(self): #, p_min = 0.1, p_max = 0.3): #p_best = 0.6, best = 2):
         #self.best = [best]
         self.num_arms = 2
+        self.seedling = None
         self.buildDecks(25, 5, 2000, 5, 1.1, -40, 5)
         self.deck_counters = np.zeros(len(self.all_cards), dtype = int)
 
